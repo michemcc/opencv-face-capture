92d47db3eca57d24886152c25092d42e8e721a31
Michael McCarthy <michemcc@outlook.com>
2023-02-23 06:25:32 -0500

    Update admin

--- capture/admin.py
+++ capture/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import ImageUploadModel
-# Register your models here.
 
 class ImageUploadAdmin(admin.ModelAdmin):
     list_display = ('description', 'document', )
